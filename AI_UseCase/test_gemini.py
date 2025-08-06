5ee149529f34cb3c51811267b07d853d07efd09b
Mayankm1411 <mayank.mehra@msds.christuniversity.in>
2025-08-06 19:55:21 +0530

    test_gemini.py

--- AI_UseCase/test_gemini.py
+++ AI_UseCase/test_gemini.py
@@ -1,9 +1,3 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBntoybBym3lKOp3DoTqc59TWKoYnqnE8I")
-
-model = genai.GenerativeModel("gemini-pro")
-response = model.generate_content("What is the capital of France?")
-print(response.text)
 
 
